ef0b007e3a09c07d411b4a91f48ef69dc6f35b86
dwi2 <dwi2@dwi2.com>
2021-12-11 20:15:49 +0900

    feat: Day 11 part 1

---
+++ day11/part-one.js
@@ -0,0 +1,109 @@
+const readline = require("readline");
+const fs = require("fs");
+
+const readInterface = readline.createInterface({
+  input: fs.createReadStream("./input"),
+  // output: process.stdout,
+  console: false,
+});
+
+const octopuses = [];
+readInterface.on("line", (line) => {
+  octopuses.push(line);
+});
+
+const print = () => {
+  console.log("===");
+  octopuses.forEach(line => {
+    console.log(line);
+  });
+  
+};
+
+const queueOfFlash = [];
+const flash = (x, y) => {
+  const X_LIMIT = octopuses[0].length;
+  const Y_LIMIT = octopuses.length;
+  const adjacents = [
+    {
+      x: x - 1,
+      y: y - 1,
+    },
+    {
+      x: x - 1,
+      y: y,
+    },
+    {
+      x: x - 1,
+      y: y + 1,
+    },
+    {
+      x: x,
+      y: y - 1,
+    },
+    {
+      x: x,
+      y: y + 1,
+    },
+    {
+      x: x + 1,
+      y: y - 1,
+    },
+    {
+      x: x + 1,
+      y: y,
+    },
+    {
+      x: x + 1,
+      y: y + 1,
+    },
+  ];
+
+  adjacents.forEach(coord => {
+    if (coord.x >= 0 && coord.x < X_LIMIT && coord.y >= 0 && coord.y < Y_LIMIT) {
+      const energy = octopuses[coord.y][coord.x];
+      if (energy !== '0') {
+        const newValue = String((Number.parseInt(energy) + 1) % 10);
+        if (newValue === '0') {
+          // console.log(`(${coord.x}, ${coord.y}) flashes`);
+          queueOfFlash.push({
+            x: coord.x,
+            y: coord.y
+          })
+        }
+        const line = octopuses[coord.y].split("");
+        line.splice(coord.x, 1, newValue);
+        octopuses[coord.y] = line.join("");
+      }
+    }
+  });
+
+};
+
+readInterface.on("close", () => {
+  const STEP_LIMIT = 100;
+  let flashCount = 0;
+  for (let step = 0; step < STEP_LIMIT; step += 1) {
+    octopuses.forEach((line, y) => {
+      const states = line.split("").map(n => Number.parseInt(n));
+      states.forEach((energy, x) => {
+        states[x] = (energy + 1) % 10;
+        if (states[x] === 0) {
+          queueOfFlash.push({ x, y });
+        }
+      });
+      octopuses[y] = states.map(n => String(n)).join("");
+    });
+
+    while (queueOfFlash.length > 0) {
+      const toFlash = queueOfFlash.pop();
+      flash(toFlash.x, toFlash.y);
+      flashCount += 1;
+      // console.log(`queueOfFlash.length = ${queueOfFlash.length}`);
+    }
+    print();
+  }
+
+  console.log(flashCount);
+
+});
